58e657bb8a9749fa093eaa5305f14ca4bcc7e0b4
El Ouissi Fares <fares.elouissi@esprit.tn>
2023-01-02 17:20:59 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map, startWith, catchError, of, BehaviorSubject } from 'rxjs';
+import { ApiResponse } from './interface/api-response';
+import { Page } from './interface/page';
+import { UserService } from './services/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  userState$: Observable<{appState: string, appData?: ApiResponse<Page>, error?: HttpErrorResponse}>;
+  responseSubject = new BehaviorSubject<ApiResponse<Page>>(null);
+  private currentPageSubject = new BehaviorSubject<number>(null);
+  currentPage$ = this.currentPageSubject.asObservable();
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userState$ = this.userService.users$().pipe(
+      map((response: ApiResponse<Page>) => {
+        this.responseSubject.next(response);
+        this.currentPageSubject.next(response.data.page.number);
+        console.log(response);
+        return ({appState: 'APP_LOADED', appData: response});
+      }),
+    startWith({appState: 'APP_LOADING'}),
+    catchError((error: HttpErrorResponse) => of({appState: 'APP_ERROR', error}))
+    )}
+
+    goToPage(gender?: string, status?: string, pageNumber: number = 0): void {
+      console.log('status ', status)
+      this.userState$ = this.userService.users$(gender, status, pageNumber).pipe(
+        map((response: ApiResponse<Page>) => {
+          this.responseSubject.next(response);
+          this.currentPageSubject.next(pageNumber)
+          console.log(response);
+          return ({appState: 'APP_LOADED', appData: response});
+        }),
+      startWith({appState: 'APP_LOADED', appData: this.responseSubject.value}),
+      catchError((error: HttpErrorResponse) => of({appState: 'APP_ERROR', error}))
+      )}
+
+    goToNextOrPrevious(direction?: string, status?: string, gender?: string): void {
+      this.goToPage(gender, status, direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1);
+    }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../interface/api-response';
+import { Page } from '../interface/page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly serverUrl: string = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) { }
+
+  // Make call to the backend API to retrieve page of users
+  users$ = (gender: string = '', status: string= '', page: number = 0):Observable<ApiResponse<Page>>  =>
+    this.http.get<any>(`${this.serverUrl}/users?gender=${gender}&status=${status}&page=${page}`);
+}
